e2e6dc0f594b05182388e80800a7ecca9943239f
Milton Araújo <37994575+whr19@users.noreply.github.com>
2019-03-31 21:43:02 -0400

    Update

--- pkg-wpad/files/install_wpad_24.sh
+++ pkg-wpad/files/install_wpad_24.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-pkg add https://github.com/whr19/Unofficial-pfSense-packages/raw/master/repo/FreeBSD:11:amd64/pfSense-pkg-WPAD-0.3.0.2.txz
+pkg add https://github.com/whr19/Secure-Tecnologia/raw/master/repo/FreeBSD-11-amd64/pfSense-pkg-WPAD-0.3.0.2.txz
